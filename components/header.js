703c1c077b0b7293c5c1b1b46047b714e7fbd382
Aceofthehood1 <geoffreyameble@gmail.com>
2023-08-19 15:23:57 +0100

    Made slight changes to colour scheme of navigation bar and footer content and placed all relevant information on one page in sections. Added a contact and about me section.

---
+++ components/header.js
@@ -0,0 +1,24 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback(){
+        this.innerHTML = `
+          <header>
+        <div class="container">
+          <nav>
+            <ul>
+              <li><a href="#welcome">Home</a></li>
+              <li><a href="#grid-title">Portfolio</a></li>
+              <li><a href="#about-title">About Me</a></li>
+              <li><a href="#contacts-title">Contacts</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+        `;
+      }
+  }
+
+  customElements.define('header-component', Header);
